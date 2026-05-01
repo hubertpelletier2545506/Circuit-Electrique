59c02dcdf2ea4497baa5d535974b53e8bc8f521f
hubertpelletier2545506 <2545506@cegeplimoilou.ca>
2026-05-01 12:57:11 -0400

    Ajustement de circuit serie constructeur.

--- src/composants/CircuitSerie.java
+++ src/composants/CircuitSerie.java
@@ -5,8 +5,8 @@ import enums.Voltage;
 import java.util.List;
 
 public class CircuitSerie extends Circuit {
-    CircuitSerie(List<Composant> composants, Voltage voltage, Protection protection) {
-        super(composants, voltage, protection);
+    CircuitSerie(List<Composant> composants, Voltage voltage, Protection protection,boolean interrupteurAllume) {
+        super(composants, voltage, protection,interrupteurAllume);
     }
 
     @Override
