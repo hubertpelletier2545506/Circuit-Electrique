81485d0762e3e7ba96dac28c2581613318c3d75f
Hubert <2545506@cegeplimoilou.ca>
2026-05-15 12:58:50 -0400

    Ajout de la javadoc pour CircuitSauteException

--- src/exception/CircuitSauteException.java
+++ src/exception/CircuitSauteException.java
@@ -1,5 +1,13 @@
 package exception;
-
+/**
+ * Exception levée lorsqu’un circuit saute (surcharge ou défaillance).
+ *
+ * Cette exception est de type non vérifiée (RuntimeException),
+ * donc elle n’a pas besoin d’être déclarée dans les signatures de méthodes.
+ *
+ * @author code Hubert
+ * @author Javadoc ChatGPT par Hubert
+ */
 public class CircuitSauteException extends RuntimeException {
     public CircuitSauteException(String message) {
         super(message);
